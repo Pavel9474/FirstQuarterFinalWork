7892429379404f957a5a41b2871a5edd1e66fc87
pavel.sharagin.1994@gmail.com <pavel.sharagin.1994@gmail.com>
2022-10-26 11:26:24 +0500

    Add GetStringArrayFromConsole

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива 
+// строк формирует новый массив из строк,
+// длина которых меньше, либо равна 3 символам. 
+string[] GetStringArrayFromConsole()
+{
+    Console.Write("Введите количество строк, которые хотите ввести: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    string[] strarr = new string[n]; 
+    for (int i = 0; i < n; i++)
+    {
+    Console.Write("Введите элемент массива №{0}: ", i + 1);
+    strarr[i] = Console.ReadLine();
+    }
+    return strarr;
+}
